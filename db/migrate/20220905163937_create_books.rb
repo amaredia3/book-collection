762ef59b1e67087cd39d57c4c9f3162e3077f466
Asim Maredia <asimmaredia@Asims-MacBook-Pro.local>
2022-09-15 14:06:40 -0500

    books CRUD

--- db/migrate/20220905163937_create_books.rb
+++ db/migrate/20220905163937_create_books.rb
@@ -2,7 +2,9 @@ class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
       t.string :title
-
+      t.stirng :author
+      t.numeric :price
+      t.date :published
       t.timestamps
     end
   end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'harry potter', author: 'JK Rowling', price: 39.99, published: '01/01/2005')
   end
 
   it 'is valid with valid attributes' do
@@ -14,4 +14,20 @@ RSpec.describe Book, type: :model do
     subject.title = nil
     expect(subject).not_to be_valid
   end
+
+  it 'is not valid without an author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a publish date' do
+    subject.published = nil
+    expect(subject).not_to be_valid
+  end
+
 end
