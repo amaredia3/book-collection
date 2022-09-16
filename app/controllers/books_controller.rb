c9654587cd0457cdc6037e422c0d8ec67de5596f
Asim Maredia <70919443+amaredia3@users.noreply.github.com>
2022-09-15 23:42:03 -0500

    delete page (#11)
    * books CRUD
    
    * patch error
    
    * fix date picker
    
    * fix integration tests
    
    * modify home and show pages
    
    * fix tests
    
    * delete page
    
    Co-authored-by: Asim Maredia <asimmaredia@Asims-MacBook-Pro.local>

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,11 @@
 class BooksController < ApplicationController
   before_action :set_book, only: %i[ show edit update destroy ]
 
+
+  def delete
+    @book = Book.find(params[:id])
+  end
+
   # GET /books or /books.json
   def index
     @books = Book.all
