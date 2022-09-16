1ec84c5e16bcea7dbd390e85b54189951f490d63
Asim Maredia <asimmaredia@Asims-MacBook-Pro.local>
2022-09-15 20:04:08 -0500

    fix integration tests

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -4,12 +4,17 @@ require 'rails_helper'
 RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
-    fill_in 'Title', with: 'harry potter'
+    fill_in 'Title', with: 'Harry Potter'
     fill_in 'Author', with: 'JK Rowling'
     fill_in 'Price', with: 39.99
-    fill_in 'Published', with: '01/01/2005'
+    select '2005', :from => 'book_published_1i'
+    select 'February', :from => 'book_published_2i'
+    select '1', :from => 'book_published_3i'
     click_on 'Create Book'
     visit books_path
-    expect(page).to have_content('harry potter')
+    expect(page).to have_content('Harry Potter')
+    expect(page).to have_content('JK Rowling')
+    expect(page).to have_content('39.99')
+    expect(page).to have_content('2005-02-01')
   end
 end
